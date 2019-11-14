de38ae3d47c2b2de26365ec77ab62f527c9bb8b5
Johannes Passing <jpassing@google.com>
2019-11-14 12:53:15 +0100

    Simplify code base
    * Delete client code as it is not needed for tutorial
    * Simplify folder structure
    * Add requirement.txt for server
    * Remove text from README that is redundant to the tutorial
    * Improve error handling
    * Clean up code
    
    Change-Id: Iaf3ed7c6bdc4c057d7735bc99828f14a370d2464

--- token_broker.py
+++ token_broker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2018 Google Inc.
+# Copyright 2019 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,95 +15,119 @@
 # limitations under the License.
 
 import socket
-from http.server import BaseHTTPRequestHandler,HTTPServer
 import ssl
 import sys
 import logging
 import os
 import traceback
+import json
+from http.server import BaseHTTPRequestHandler,HTTPServer
 from google.auth.transport.requests import AuthorizedSession
 from google import auth
-import json
-
-# See README.md file for instructions
-# HTTPS server address
-SERVER = sys.argv[1]
-# HTTPS server port
-SERVER_PORT = int(sys.argv[2])
-# Server certificate location (must include the private key)
-SERVER_CERTIFICATE = sys.argv[3]
-# Service account for which a token is generated
-SERVICE_ACCOUNT = sys.argv[4]
-# Certificate bundle
-CA_BUNDLE = sys.argv[5]
-os.environ['REQUESTS_CA_BUNDLE'] = CA_BUNDLE
-
-# Generates an access token based on the service account attached to the GCE instance
-def get_access_token():
-    credentials, project = auth.default()
-    authed_session = AuthorizedSession(credentials)
-    # You can limit scope and lifetime of the token
-    logging.info("Generating token")
-    response = authed_session.post('https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/' + SERVICE_ACCOUNT + ':generateAccessToken', data={"lifetime": "600s" ,"scope" : ["https://www.googleapis.com/auth/cloud-platform"]})
-    return json.loads(response.content.decode("utf-8"))["accessToken"]
 
 class LoggingSSLSocket(ssl.SSLSocket):
-
     def accept(self, *args, **kwargs):
-        logging.info('Accepting connection')
+        logging.info("Accepting connection")
+
         try:
             result = super(LoggingSSLSocket, self).accept(*args, **kwargs)
+            logging.info("Accepted connection.")
         except:
             logging.info("Connection error: " + traceback.format_exc())
             raise
-        logging.info('Done accepting connection.')
+
         return result
 
     def do_handshake(self, *args, **kwargs):
-        logging.info('Starting handshake')
+        logging.info("Starting handshake")
+        
         try:
             result = super(LoggingSSLSocket, self).do_handshake(*args, **kwargs)
+            logging.info("Handshake complered")
         except:
             logging.info("Handshake error: " + traceback.format_exc())
             raise
-        logging.info('Done with handshake.')
+            
         return result
 
 class Handler(BaseHTTPRequestHandler):
+    # Token lifetime in seconds, maximum one hour.
+    TOKEN_LIFETIME = 600
+    
+    # Scopes to use for token.
+    TOKEN_SCOPE = "https://www.googleapis.com/auth/cloud-platform"
+
+    def create_access_token(self):
+        credentials, project = auth.default()
+        
+        response = AuthorizedSession(credentials).post(
+            "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/" + Handler.role_account + ":generateAccessToken", 
+            data={
+                "lifetime": "%ds" % Handler.TOKEN_LIFETIME,
+                "scope" : [Handler.TOKEN_SCOPE]})
+                
+        return json.loads(response.content.decode("utf-8"))["accessToken"]
 
     def log_message(self, format, *args):
-        logging.info(format%args)
+        logging.info(format % args)
 
     def log_error(self, format, *args):
-        logging.error(format%args)
+        logging.error(format % args)
 
     def do_GET(self):
-        cert = self.connection.getpeercert()
-        logging.info("Client certificate: " + str(cert))
+        try:
+            cert = self.connection.getpeercert()
+            logging.info("Client certificate: " + str(cert))
 
-        # Verify request origin
-        ip = self.connection.getpeername()[0]
-        host = socket.gethostbyaddr(ip)
-        ssl.match_hostname(cert, host[0])
+            # Authenticate client.
+            ip = self.connection.getpeername()[0]
+            host = socket.gethostbyaddr(ip)
+            ssl.match_hostname(cert, host[0])
+            
+            logging.info("Client %s authenticated" % str(host))
+            
+            if self.path == "/token":
+                token = self.create_access_token()
+            
+                self.send_response(200)
+                self.wfile.write(token.encode("utf-8"))
+                
+            else:
+                self.send_error(404)
+                
+        except ssl.CertificateError as e:
+            logging.exception("Certificate validation failed")
+            self.send_error(403)
         
-        logging.info("Client host: " + str(host))
+        except Exception as e:
+            logging.exception("Failed to handle request")
+            self.send_error(500)
+
+if __name__ == "__main__":
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+    if len(sys.argv) < 6:
+        print("Usage: %s listen-ip listen-port server-certitficate role-account ca-certificate")
+        sys.exit(1)
         
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
+    listen_ip = sys.argv[1]
+    listen_port = int(sys.argv[2])
+    server_cert = sys.argv[3]
+    role_account = sys.argv[4]
+    ca_cert = sys.argv[5]
 
-        if self.path == '/token':
-            token = get_access_token()
-            self.wfile.write(token.encode("utf-8"))
-        return
+    # Create HTTPS server on port 443
+    Handler.role_account = role_account
+    httpd = HTTPServer((listen_ip, listen_port), Handler)
+    
+    # Create a socket and check client certificate for TLS handshake. 
+    httpd.socket = LoggingSSLSocket(
+        httpd.socket, 
+        certfile = server_cert, 
+        server_side = True, 
+        cert_reqs = ssl.CERT_REQUIRED, 
+        ca_certs = ca_cert)
+        
+    logging.info("Starting server on %s:%s" % (listen_port, listen_port))
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-
-# Create HTTPS server on port 443
-httpd = HTTPServer((SERVER, SERVER_PORT), Handler)
-# Create a socket and check client certificate for TLS handshake. ca_certs parameter points to the file with CA certificates.
-httpd.socket = LoggingSSLSocket(httpd.socket, certfile = SERVER_CERTIFICATE, server_side = True, cert_reqs = ssl.CERT_REQUIRED, ca_certs = CA_BUNDLE)
-logging.info("Starting server on " + SERVER + ":" + str(SERVER_PORT))
-
-# Start the server
-httpd.serve_forever()
+    httpd.serve_forever()
